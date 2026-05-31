253222b6a3fbb14016b6fd860016d7786e4a8372
kirillgrigorev94-dev <kirill.grigorev.94@bk.ru>
2026-05-31 18:23:25 +1000

    Importing libraries and setting up logging

---
+++ parsed.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import logging
+import json
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
